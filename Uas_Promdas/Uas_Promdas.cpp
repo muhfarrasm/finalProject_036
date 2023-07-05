c503a6998cc76307b2967bbb18dae81e5d753684
muhfarrasm <muhammadfarrasm@gmail.com>
2023-07-05 15:39:40 +0700

    Bismilah nilai A

--- Uas_Promdas/Uas_Promdas.cpp
+++ Uas_Promdas/Uas_Promdas.cpp
@@ -1,20 +1,111 @@
-// Uas_Promdas.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+class MataKuliah {
+private:
+	float presensi;
+	float activity;
+	float exercise;
+	float tugasAkhir;
+public:
+	MataKuliah() {
+		presensi = 0;
+		activity = 0;
+		exercise = 0;
+		tugasAkhir = 0;
+	}
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	virtual float hitungNilaiAkhir() { return 0; }
+	virtual void cekKelulusan() {	return;}
+	virtual void input() { return; }
+
+	void setP(float nilai) {
+		this->presensi = nilai;
+	}
+	float getP() {
+		return presensi;
+	}
+	void setA(float nilai) {
+		this->activity = nilai;
+	}
+	float getA() {
+		return activity;
+	}
+
+	void setE(float nilai) {
+		this->exercise = nilai;
+	}
+	float getE() {
+		return exercise;
+	}
+	void setUA(float nilai) {
+		this->tugasAkhir = nilai;
+	}
+	float getUA() {
+		return tugasAkhir;
+	}
+	
+};
+
+
+class Pemrograman : public MataKuliah { 
+public:
+	float hitungNilaiAkhir() {
+		return ((getP() * 0.1) + (getA() * 0.2) + (getE() * 0.3) + (getUA() * 0.4));
+	}
+
+	void cekKelulusan() {
+		float nilai_akhir = hitungNilaiAkhir();
+		if (nilai_akhir >= 75) {
+			cout << "Selamat, anda lulus mata kuliah pemrograman dengan nilai akhir : " << nilai_akhir << "\n";
+			
+		}
+		else {
+			cout << "Maaf, anda tidak lulus mata kuliah pemrograman dengan nilai akhir : " << nilai_akhir << "\n";
+		}
+		
+	}
+
+	void input() {
+		cout << "Masukkan nilai Presensi: ";
+		float presensi;
+		cin >> presensi;
+		setP(presensi);
+
+		cout << "Masukkan nilai Activity: ";
+		float activity;
+		cin >> activity;
+		setA(activity);
+
+		cout << "Masukkan nilai Exercise: ";
+		float exercise;
+		cin >> exercise;
+		setE(exercise);
+
+		cout << "Masukkan nilai Tugas Akhir: ";
+		float tugasAkhir;
+		cin >> tugasAkhir;
+		setUA(tugasAkhir);
+	}
+
+};
+
+int main() { 
+	char ulang;
+
+	do 
+	{
+		cout << "Program dibuat\n";
+		Pemrograman pemrograman;
+		pemrograman.input();
+		pemrograman.cekKelulusan();
+
+		cout << "Apakah Anda ingin mengulang program?? Tekan Y/N \n";
+		cin >> ulang;
+	
+
+
+	} while (ulang == 'Y' || ulang == 'y');
+
+	return 0;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
